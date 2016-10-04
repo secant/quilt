903e987ed1c02cc4915fcc1e2cbc204c5cfaedf8
Vivian Fang <vivian98.fang@gmail.com>
2016-10-04 13:19:47 -0700

    specs: Rewrite Spark spec

--- specs/spark/spark.js
+++ specs/spark/spark.js
@@ -1,61 +1,48 @@
-var image = "quilt/spark";
+var Image = "quilt/spark";
 
-function commaSepHosts(labels) {
-    return _(labels).map(Label.prototype.hostname).join(",");
-}
-
-function createMasters(n, zookeeper) {
-    var containers = new Docker(image, ["run", "master"]).replicate(n);
-
-    if (zookeeper) {
-        var zookeeperHosts = commaSepHosts(zookeeper);
-        for (var i = 0 ; i<containers.length ; i++) {
-            containers[i].setEnv("ZOO", zookeeperHosts);
-        }
-    }
-
-    return new Label("spark-ms", containers);
-}
-
-function createWorkers(n, masters) {
-    var masterHosts = masters.children().join(",");
-    var containers = new Docker(image, ["run", "worker"])
-        .withEnv({"MASTERS": masterHosts})
-        .replicate(n);
-
-    return new Label("spark-wk", containers);
-}
-
-function link(masters, workers, zookeeper) {
-    var allPorts = new PortRange(1000, 65535);
-    connect(allPorts, workers, masters);
-    connect(allPorts, masters, workers);
-    connect(new Port(7077), workers, masters);
-    if (zookeeper) {
-        connect(new Port(2181), masters, zookeeper);
-    }
+function SetImage(newImage) {
+    Image = newImage;
 }
 
 function Spark(nMaster, nWorker, zookeeper) {
-    this.masters = createMasters(nMaster, zookeeper);
-    this.workers = createWorkers(nWorker, this.masters);
-    link(this.masters, this.workers, zookeeper);
-}
+    var dkms = new Docker(Image, ["master"])
+        .replicate(nMaster);
+    
+    if (zookeeper) {
+        var zooHosts = zookeeper.children().join(",");
+        for (var i = 0; i < nMaster; i++) {
+            dkms[i].setEnv("ZOO", zooHosts);
+        }
+    }
 
-Spark.prototype.job = function(command) {
-    var masters = this.masters.containers;
-    for (var i = 0 ; i<masters.length ; i++) {
-        masters[i].env["JOB"] = command;
+    this.masters = new Label("spark-ms", dkms);
+    
+    var dkws = new Docker(Image, ["worker"])
+        .withEnv({"MASTERS": this.masters.children().join(",")})
+        .replicate(nWorker);
+    this.workers = new Label("spark-wk", dkws);
+
+    connect(7077, this.workers, this.workers);
+    connect(7077, this.workers, this.masters);
+    if (zookeeper) {
+        connect(2181, this.masters, zookeeper);
+    }
+    
+    this.job = function(command) {
+        var cnt = this.masters.containers;
+        for (var i = 0; i < cnt.length; i++) {
+            cnt[i].env["JOB"] = command;
+        }
+    }
+    this.public = function() {
+        connect(new Port(8080), publicInternet, this.masters);
+        connect(new Port(8081), publicInternet, this.workers);
+    }
+
+    this.exclusive = function() {
+        place(this.masters, new LabelRule("exclusive", this.workers));
     }
 }
 
-Spark.prototype.public = function() {
-    connect(new Port(8080), publicInternet, this.masters);
-    connect(new Port(8081), publicInternet, this.workers);
-}
-
-Spark.prototype.exclusive = function(sparkMap) {
-    place(this.masters, new LabelRule("exclusive", this.workers))
-}
-
-module.exports = Spark;
+exports.SetImage = SetImage;
+exports.Spark = Spark;
--- specs/spark/sparkPI.js
+++ specs/spark/sparkPI.js
@@ -1,15 +1,18 @@
 // Import spark.spec
-var Spark = require("github.com/NetSys/quilt/specs/spark/spark");
+var Spark = require("spark");
+
+// Set the image to be Spark 1.5
+Spark.SetImage("vfang/spark:1.5.2");
 
 // We will have three worker machines.
 var nWorker = 3;
 
 // Application
-// spark.Exclusive enforces that no two Spark containers should be on the
-// same node. spark.Public says that the containers should be allowed to talk
-// on the public internet. spark.Job causes Spark to run that job when it
+// sprk.exclusive enforces that no two Spark containers should be on the
+// same node. sprk.public says that the containers should be allowed to talk
+// on the public internet. sprk.job causes Spark to run that job when it
 // boots.
-var sprk = new Spark(1, nWorker);
+var sprk = new Spark.Spark(1, nWorker);
 sprk.exclusive()
 sprk.public()
 sprk.job("run-example SparkPi");
@@ -18,7 +21,7 @@ sprk.job("run-example SparkPi");
 
 // Using unique Namespaces will allow multiple Quilt instances to run on the
 // same cloud provider account without conflict.
-setNamespace("kklin");
+setNamespace("CHANGE_ME");
 
 // Defines the set of addresses that are allowed to access Quilt VMs.
 setAdminACL(["local"]);
@@ -28,7 +31,7 @@ var baseMachine = new Machine({
     region: "us-west-1",
     size: "m4.large",
     diskSize: 32,
-    keys: githubKeys("kklin"),
+    keys: githubKeys("CHANGE_ME"),
 });
 deployWorkers(nWorker + 1, baseMachine);
 deployMasters(1, baseMachine);
